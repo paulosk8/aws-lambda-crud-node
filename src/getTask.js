7a687b1a89e8169ce1d69352c9c00a5eb085df34
Paulo Galarza <paulo-123@hotmail.es>
2024-12-02 09:11:34 -0500

    second commit: Listar tareas creadas en la tabla TaskTable

---
+++ src/getTask.js
@@ -0,0 +1,17 @@
+const AWS = require("aws-sdk");
+
+exports.getTask = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const result = await dynamodb
+    .scan({
+      TableName: "TaskTable",
+    })
+    .promise();
+  const tasks = result.Items;
+  return {
+    status: 200,
+    body: {
+      tasks,
+    },
+  };
+};
